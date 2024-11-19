0252622575af01d39205fc70ce1348e449424afd
KaiWong1 <kwong1@ltu.edu>
2024-11-19 04:17:01 -0500

    init commit test

--- esp32_weather/src/main.cpp
+++ esp32_weather/src/main.cpp
@@ -2,9 +2,7 @@
 Kai Wong
 
 References: https://randomnerdtutorials.com/esp32-tft-lvgl-weather-station/
-
 */
-
 
 #include "SPI.h"
 #include "Adafruit_GFX.h"
@@ -13,13 +11,13 @@ References: https://randomnerdtutorials.com/esp32-tft-lvgl-weather-station/
 #include "HTTPClient.h"
 #include "ArduinoJson.h"
 
-// For the Adafruit shield, these are the default.
+// For the Adafruit shield, these are the default pins.
 #define TFT_DC 4
 #define TFT_CS 15
-#define TFT_RST 2 //disconnect this pin when uploading.
-#define TFT_MISO 19         
-#define TFT_MOSI 23           
-#define TFT_CLK 18 
+#define TFT_RST 2 // Disconnect this pin when uploading.
+#define TFT_MISO 19
+#define TFT_MOSI 23
+#define TFT_CLK 18
 
 Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC, TFT_MOSI, TFT_CLK, TFT_RST, TFT_MISO);
 
@@ -39,9 +37,9 @@ int is_day;
 int weather_code = 0;
 String weather_description;
 
-#define TEMP_CELCIUS 0;
+#define TEMP_CELCIUS 0
 
-#if TEMP_CELSIUS
+#if TEMP_CELCIUS
   String temperature_unit = "";
   const char degree_symbol[] = "\u00B0C";
 #else
@@ -49,145 +47,107 @@ String weather_description;
   const char degree_symbol[] = "\u00B0F";
 #endif
 
-
 void setup() {
-
-  //Serial.begin(9600);
   Serial.begin(115200);
-  Serial.println("Booting"); 
- 
-  //connect to wifi
-  WiFi.begin(ssid, password);
-  Serial.print("connecting");
-  
+  Serial.println("Booting");
+
+  // Initialize TFT
   tft.begin();
-  //if esp32 connects to internet, go to loop
-  //if else, stay on bootUp();
-  if (WiFi.status() == WL_CONNECTED)
-  {
+  tft.fillScreen(ILI9341_BLACK);
+  tft.setRotation(0);
+
+  // Connect to Wi-Fi
+  connectToInternet();
+
+  if (WiFi.status() == WL_CONNECTED) {
     getWeatherData();
-  }
-  else
-  {
-  bootUp();
+  } else {
+    bootUp();
   }
 }
-  /* Shows the bootup screen when the ESP32 is connecting to the internet. 
-  After connecting to the internet the bootup screen will go away and load
-  the weather related GUI elements. 
-  */
-unsigned long bootUp() 
-{
+
+void bootUp() {
   tft.fillScreen(ILI9341_BLACK);
-  unsigned long start = micros();
-  tft.setCursor(0,100);
+  tft.setCursor(0, 100);
   tft.setTextColor(ILI9341_GREEN);
   tft.setTextSize(3);
   tft.println("Connecting to");
   tft.println("the internet!");
-  return micros() - start;
 
-  if (WiFi.status() == WL_CONNECTED)
-  {
-    getWeatherData();
-  }
-  else
-  {
-    bootUp();
-  }
-    
+  connectToInternet();
 }
 
-unsigned long weatherGUI()
-{
-
-}
-
-unsigned long connectToInternet()
-{
-  {
+void connectToInternet() {
   WiFi.begin(ssid, password);
   Serial.print("Connecting");
-  while (WiFi.status() != WL_CONNECTED) 
-    {
+
+  while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
-    }
   }
 
-Serial.print("\nConnected to Wi-Fi network with IP Address: ");
-Serial.println(WiFi.localIP());
-
+  Serial.print("\nConnected to Wi-Fi network with IP Address: ");
+  Serial.println(WiFi.localIP());
 }
 
-void getWeatherData()
-{
-  if (WiFi.status() == WL_CONNECTED) 
-  {
+void getWeatherData() {
+  if (WiFi.status() == WL_CONNECTED) {
     HTTPClient http;
-    // Construct the API endpoint
-    String url = String("http://api.open-meteo.com/v1/forecast?latitude=" + latitude + "&longitude=" + longitude + "&current=temperature_2m,relative_humidity_2m,is_day,precipitation,rain,weather_code" + temperature_unit + "&timezone=" + timezone + "&forecast_days=1");
-    http.begin(url);
-    int httpCode = http.GET(); // Make the GET request
+    String url = "http://api.open-meteo.com/v1/forecast?latitude=" + latitude +
+                 "&longitude=" + longitude +
+                 "&current_weather=true&timezone=" + timezone + temperature_unit;
 
-    if (httpCode > 0) 
-    {
-      // Check for the response
-      if (httpCode == HTTP_CODE_OK) 
-      {
+    http.begin(url);
+    int httpCode = http.GET();
+
+    if (httpCode > 0) {
+      if (httpCode == HTTP_CODE_OK) {
         String payload = http.getString();
-        //Serial.println("Request information:");
-        //Serial.println(payload);
-        // Parse the JSON to extract the time
-        JsonDocument doc;
+        StaticJsonDocument<1024> doc;
+
         DeserializationError error = deserializeJson(doc, payload);
-        if (!error) 
-        {
-          const char* datetime = doc["current"]["time"];
-          temperature = String(doc["current"]["temperature_2m"]);
-          humidity = String(doc["current"]["relative_humidity_2m"]);
-          is_day = String(doc["current"]["is_day"]).toInt();
-          weather_code = String(doc["current"]["weather_code"]).toInt();
-          /*Serial.println(temperature);
-          Serial.println(humidity);
-          Serial.println(is_day);
-          Serial.println(weather_code);
-          Serial.println(String(timezone));*/
-          // Split the datetime into date and time
-          String datetime_str = String(datetime);
-          int splitIndex = datetime_str.indexOf('T');
-          current_date = datetime_str.substring(0, splitIndex);
-          last_weather_update = datetime_str.substring(splitIndex + 1, splitIndex + 9); // Extract time portion
-        } 
-        else 
-        {
-          Serial.print("deserializeJson() failed: ");
+        if (!error) {
+          temperature = String(doc["current_weather"]["temperature"]);
+          humidity = String(doc["current_weather"]["humidity"]);
+          is_day = doc["current_weather"]["is_day"];
+          weather_code = doc["current_weather"]["weather_code"];
+
+          Serial.println("Weather Data Retrieved:");
+          Serial.println("Temperature: " + temperature + degree_symbol);
+          Serial.println("Humidity: " + humidity);
+        } else {
+          Serial.print("JSON Parsing failed: ");
           Serial.println(error.c_str());
         }
       }
-    } 
-    else 
-    {
+    } else {
       Serial.printf("GET request failed, error: %s\n", http.errorToString(httpCode).c_str());
     }
-    http.end(); // Close connection
-  } 
-  else 
-  {
+    http.end();
+  } else {
     Serial.println("Not connected to Wi-Fi");
   }
-
 }
 
+void loop() {
+  if (WiFi.status() != WL_CONNECTED) {
+    bootUp();
+  } else {
+    // Placeholder for weather GUI
+    weatherGUI();
+  }
 
-void loop() 
-{
-  tft.setRotation(0);
-  /*if ESP32 connects to internet proceed with GUI elements.
-  If else, then the ESP32 will keep running BootUp() in void setup(). Maybe
-  add a RTOS? */
-
-  //delay(1000);
-
+  delay(10000); // Update every 10 seconds
 }
 
+void weatherGUI() {
+  tft.fillScreen(ILI9341_BLACK);
+  tft.setCursor(10, 10);
+  tft.setTextColor(ILI9341_WHITE);
+  tft.setTextSize(2);
+
+  tft.println("Weather Information:");
+  tft.println("Location: " + location);
+  tft.println("Temperature: " + temperature + degree_symbol);
+  tft.println("Humidity: " + humidity + "%");
+}
